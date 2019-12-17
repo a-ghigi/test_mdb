09e6f54e0493378b071f21d957a2f223b9d47b63
Angelo Ghigi <angelo@ghigi.com>
2019-12-17 16:32:15 +0100

    Implementa la sezione Best Features

--- application/views/test/index.php
+++ application/views/test/index.php
@@ -1,5 +1,96 @@
 	<!--Main layout-->
-	<main>
+	<main class="mt-5">
+		<div class="container">
 
+			<!--Section: Best Features-->
+			<section id="best-features" class="text-center">
+
+				<!-- Heading -->
+				<h2 class="mb-5 font-weight-bold">Best Features</h2>
+
+				<!--Grid row-->
+				<div class="row d-flex justify-content-center mb-4">
+
+					<!--Grid column-->
+					<div class="col-md-8">
+
+						<!-- Description -->
+						<p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi voluptate hic provident nulla repellat
+							facere esse molestiae ipsa labore porro minima quam quaerat rem, natus repudiandae debitis est
+							sit pariatur.</p>
+
+					</div>
+					<!--Grid column-->
+
+				</div>
+				<!--Grid row-->
+
+				<!--Grid row-->
+				<div class="row">
+
+					<!--Grid column-->
+					<div class="col-md-4 mb-1">
+						<i class="fas fa-camera-retro fa-4x orange-text"></i>
+						<h4 class="my-4">Experience</h4>
+						<p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+							assumenda deleniti hic.</p>
+					</div>
+					<!--Grid column-->
+
+					<!--Grid column-->
+					<div class="col-md-4 mb-1">
+						<i class="fas fa-heart fa-4x orange-text"></i>
+						<h4 class="my-4">Happiness</h4>
+						<p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+							assumenda deleniti hic.</p>
+					</div>
+					<!--Grid column-->
+
+					<!--Grid column-->
+					<div class="col-md-4 mb-1">
+						<i class="fas fa-bicycle fa-4x orange-text"></i>
+						<h4 class="my-4">Adventure</h4>
+						<p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+							assumenda deleniti hic.</p>
+					</div>
+					<!--Grid column-->
+
+				</div>
+				<!--Grid row-->
+
+			</section>
+			<!--Section: Best Features-->
+
+			<hr class="my-5">
+
+			<!--Section: Examples-->
+			<section id="examples">
+
+
+
+			</section>
+			<!--Section: Examples-->
+
+			<hr class="my-5">
+
+			<!--Section: Gallery-->
+			<section id="gallery">
+
+
+
+			</section>
+			<!--Section: Gallery-->
+
+			<hr class="my-5">
+
+			<!--Section: Contact-->
+			<section id="contact">
+
+
+
+			</section>
+			<!--Section: Contact-->
+
+		</div>
 	</main>
 	<!--Main layout-->
