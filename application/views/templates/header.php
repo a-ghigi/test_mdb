e60ad74dea64943b0d300a27a2dc9dd8050eaa5d
Angelo Ghigi <angelo@ghigi.com>
2019-12-17 15:24:55 +0100

    Implementa l'esempio Corporate Website

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -24,54 +24,61 @@
 	<header>
 
 		<!--Navbar-->
-		<nav class="navbar navbar-expand-lg navbar-dark primary-color">
+		<nav class="navbar navbar-expand-lg navbar-dark indigo">
 
-		  <!-- Navbar brand -->
-		  <a class="navbar-brand" href="#">Navbar</a>
+			
+			<!-- Additional container -->
+			<div class="container">
 
-		  <!-- Collapse button -->
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
-			  aria-expanded="false" aria-label="Toggle navigation">
-			  <span class="navbar-toggler-icon"></span>
-		  </button>
+				<!-- Navbar brand -->
+				<a class="navbar-brand" href="#">Navbar</a>
 
-		  <!-- Collapsible content -->
-		  <div class="collapse navbar-collapse" id="basicExampleNav">
+				<!-- Collapse button -->
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+					aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 
-			  <!-- Links -->
-			  <ul class="navbar-nav mr-auto">
-				  <li class="nav-item active">
-					  <a class="nav-link" href="#">Home
-						  <span class="sr-only">(current)</span>
-					  </a>
-				  </li>
-				  <li class="nav-item">
-					  <a class="nav-link" href="#">Features</a>
-				  </li>
-				  <li class="nav-item">
-					  <a class="nav-link" href="#">Pricing</a>
-				  </li>
+				<!-- Collapsible content -->
+				<div class="collapse navbar-collapse" id="basicExampleNav">
 
-				  <!-- Dropdown -->
-				  <li class="nav-item dropdown">
-					  <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-					  <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-						  <a class="dropdown-item" href="#">Action</a>
-						  <a class="dropdown-item" href="#">Another action</a>
-						  <a class="dropdown-item" href="#">Something else here</a>
-					  </div>
-				  </li>
+					<!-- Links -->
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home
+								<span class="sr-only">(current)</span>
+							</a>
+							</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						</li>
 
-			  </ul>
-			  <!-- Links -->
+						<!-- Dropdown -->
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+								<div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<a class="dropdown-item" href="#">Something else here</a>
+									</div>
+								</li>
 
-			  <form class="form-inline">
-				  <div class="md-form my-0">
-					  <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-				  </div>
-			  </form>
-		  </div>
-		  <!-- Collapsible content -->
+					</ul>
+					<!-- Links -->
+
+					<form class="form-inline">
+						<div class="md-form my-0">
+							<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+						</div>
+					</form>
+				</div>
+				<!-- Collapsible content -->
+
+			</div>
+			<!-- Additional container -->
 
 		</nav>
 		<!--/.Navbar-->
--- application/views/test/index.php
+++ application/views/test/index.php
@@ -1,57 +1,134 @@
-	<!--Main container-->
-	<div class="container">
+	<!--Main layout-->
+	<main class="mt-5">
 
-		<!--Grid row-->
-		<div class="row">
+		<!--Main container-->
+		<div class="container">
 
-			<!--Grid column-->
-			<div class="col-md-7">
+			<!--Grid row-->
+			<div class="row">
 
+				<!--Grid column-->
+				<div class="col-md-7 mb-4">
 
+					<div class="view overlay z-depth-1-half">
+						<img src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" class="card-img-top" alt="">
+						<div class="mask rgba-white-light"></div>
+					</div>
+
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-md-5 mb-4">
+
+					<h2>Some awesome heading</h2>
+					<hr>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis pariatur quod ipsum atque quam dolorem
+						voluptate officia sunt placeat consectetur alias fugit cum praesentium ratione sint mollitia, perferendis
+						natus quaerat!</p>
+					<a href="https://mdbootstrap.com/" class="btn btn-indigo">Get it now!</a>
+
+				</div>
+				<!--Grid column-->
 
 			</div>
-			<!--Grid column-->
+			<!--Grid row-->
 
-			<!--Grid column-->
-			<div class="col-md-5">
+			<!--Grid row-->
+			<div class="row">
 
+				<!--Grid column-->
+				<div class="col-lg-4 col-md-12 mb-4">
 
+					<!--Card-->
+					<div class="card">
+
+						<!--Card image-->
+						<div class="view overlay">
+							<img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(72).jpg" class="card-img-top" alt="">
+							<a href="#">
+								<div class="mask rgba-white-slight"></div>
+							</a>
+						</div>
+
+						<!--Card content-->
+						<div class="card-body">
+							<!--Title-->
+							<h4 class="card-title">Card title</h4>
+							<!--Text-->
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="#!" class="btn btn-indigo">Button</a>
+						</div>
+						
+					</div>
+					<!--Card-->
+
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-lg-4 col-md-6 mb-4">
+
+					<!--Card-->
+					<div class="card">
+
+						<!--Card image-->
+						<div class="view overlay">
+							<img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(72).jpg" class="card-img-top" alt="">
+							<a href="#">
+								<div class="mask rgba-white-slight"></div>
+							</a>
+						</div>
+
+						<!--Card content-->
+						<div class="card-body">
+							<!--Title-->
+							<h4 class="card-title">Card title</h4>
+							<!--Text-->
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="#!" class="btn btn-indigo">Button</a>
+						</div>
+						
+					</div>
+					<!--Card-->
+					
+				</div>
+				<!--Grid column-->
+
+				<!--Grid column-->
+				<div class="col-lg-4 col-md-6 mb-4">
+
+					<!--Card-->
+					<div class="card">
+
+						<!--Card image-->
+						<div class="view overlay">
+							<img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(72).jpg" class="card-img-top" alt="">
+							<a href="#">
+								<div class="mask rgba-white-slight"></div>
+							</a>
+						</div>
+
+						<!--Card content-->
+						<div class="card-body">
+							<!--Title-->
+							<h4 class="card-title">Card title</h4>
+							<!--Text-->
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<a href="#!" class="btn btn-indigo">Button</a>
+						</div>
+						
+					</div>
+					<!--Card-->
+
+				</div>
+				<!--Grid column-->
 
 			</div>
-			<!--Grid column-->
+			<!--Grid row-->
 
 		</div>
-		<!--Grid row-->
+		<!--Main container-->
 
-		<!--Grid row-->
-		<div class="row">
-
-			<!--Grid column-->
-			<div class="col-lg-4 col-md-12">
-
-
-
-			</div>
-			<!--Grid column-->
-
-			<!--Grid column-->
-			<div class="col-lg-4 col-md-6">
-
-
-
-			</div>
-			<!--Grid column-->
-
-			<!--Grid column-->
-			<div class="col-lg-4 col-md-6">
-
-
-
-			</div>
-			<!--Grid column-->
-
-		</div>
-		<!--Grid row-->
-
-	</div>
-	<!--Main container-->
+	</main>
+	<!--Main layout-->
