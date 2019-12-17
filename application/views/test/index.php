dd72d81ae0a7b324b424684cc416ef48c2474052
Angelo Ghigi <angelo@ghigi.com>
2019-12-17 14:32:49 +0100

    Definisce la griglia del blocco centrale

--- application/views/test/index.php
+++ application/views/test/index.php
@@ -1,5 +1,57 @@
-	<!--Main layout-->
-	<main>
+	<!--Main container-->
+	<div class="container">
 
-	</main>
-	<!--Main layout-->
+		<!--Grid row-->
+		<div class="row">
+
+			<!--Grid column-->
+			<div class="col-md-7">
+
+
+
+			</div>
+			<!--Grid column-->
+
+			<!--Grid column-->
+			<div class="col-md-5">
+
+
+
+			</div>
+			<!--Grid column-->
+
+		</div>
+		<!--Grid row-->
+
+		<!--Grid row-->
+		<div class="row">
+
+			<!--Grid column-->
+			<div class="col-lg-4 col-md-12">
+
+
+
+			</div>
+			<!--Grid column-->
+
+			<!--Grid column-->
+			<div class="col-lg-4 col-md-6">
+
+
+
+			</div>
+			<!--Grid column-->
+
+			<!--Grid column-->
+			<div class="col-lg-4 col-md-6">
+
+
+
+			</div>
+			<!--Grid column-->
+
+		</div>
+		<!--Grid row-->
+
+	</div>
+	<!--Main container-->
