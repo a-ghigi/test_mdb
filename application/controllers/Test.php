09eb1de9ca270d4b6ebfdb620c00583704043428
Angelo Ghigi <angelo@ghigi.com>
2019-12-17 12:43:50 +0100

    Implementa il test base di MDB

---
+++ application/controllers/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+class Test extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->helper('url');
+	}
+	
+	
+	public function index()
+	{
+		$this->load->view('test/index');
+	}
+}
