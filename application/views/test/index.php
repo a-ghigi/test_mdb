0e5ccb179e72ad9e352c7a0f67556275588cdf74
Angelo Ghigi <angelo@ghigi.com>
2019-12-18 17:32:30 +0100

    Implementa Accordion

--- application/views/test/index.php
+++ application/views/test/index.php
@@ -10,6 +10,7 @@
 				<div class="modal-dialog modal-sm modal-notify modal-danger" role="document">
 					<!--Content-->
 					<div class="modal-content text-center">
+						
 						<!--Header-->
 						<div class="modal-header d-flex justify-content-center">
 							<p class="heading">Are you sure?</p>
@@ -27,6 +28,7 @@
 							<a href="https://mdbootstrap.com/products/jquery-ui-kit/" class="btn btn-danger">Yes</a>
 							<a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No</a>
 						</div>
+						
 					</div>
 					<!--/.Content-->
 				</div>
@@ -413,11 +415,127 @@
 				</div>
 				<!--/.Card-->
 
-			  </section>
-			  <!--Section: Table-->
+			</section>
+			<!--Section: Table-->
 
 			<!--Section: Accordion-->
 			<section class="mb-5">
+
+				<!--Accordion wrapper-->
+				<div class="md-accordion accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
+
+					<!-- Accordion card -->
+					<div class="card">
+
+						<!-- Card header -->
+						<div class="card-header" role="tab" id="headingOne">
+
+							<!--Options-->
+							<div class="dropdown float-left">
+								<button class="btn btn-info btn-sm m-0 mr-3 p-2 dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fas fa-pencil-alt"></i>
+								</button>
+								<div class="dropdown-menu dropdown-info">
+									<a class="dropdown-item" href="#">Add new</a>
+									<a class="dropdown-item" href="#">Rename folder</a>
+									<a class="dropdown-item" href="#">Delete folder</a>
+								</div>
+							</div>
+
+							<!-- Heading -->
+							<a id="folder-1" data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								<h5 class="mt-1 mb-0">
+							  <span>Folder 1</span>
+							  <i class="fas fa-angle-down rotate-icon"></i>
+							</h5>
+							</a>
+
+						</div>
+
+						<!-- Card body -->
+						<div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordionEx">
+							<div class="card-body">
+								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+							</div>
+						</div>
+					</div>
+					<!-- Accordion card -->
+
+					<!-- Accordion card -->
+					<div class="card">
+
+						<!-- Card header -->
+						<div class="card-header" role="tab" id="headingTwo">
+
+							<!--Options-->
+							<div class="dropdown float-left">
+								<button class="btn btn-info btn-sm m-0 mr-3 p-2 dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fas fa-pencil-alt"></i>
+								</button>
+								<div class="dropdown-menu dropdown-info">
+									<a class="dropdown-item" href="#">Add new</a>
+									<a class="dropdown-item" href="#">Rename folder</a>
+									<a class="dropdown-item" href="#">Delete folder</a>
+								</div>
+							</div>
+
+							<!-- Heading -->
+							<a id="folder-1" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+								<h5 class="mt-1 mb-0">
+							  <span>Folder 2</span>
+							  <i class="fas fa-angle-down rotate-icon"></i>
+							</h5>
+							</a>
+
+						</div>
+
+						<!-- Card body -->
+						<div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordionEx">
+							<div class="card-body">
+								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+							</div>
+						</div>
+					</div>
+					<!-- Accordion card -->
+
+					<!-- Accordion card -->
+					<div class="card">
+
+						<!-- Card header -->
+						<div class="card-header" role="tab" id="headingThree">
+
+							<!--Options-->
+							<div class="dropdown float-left">
+								<button class="btn btn-info btn-sm m-0 mr-3 p-2 dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fas fa-pencil-alt"></i>
+								</button>
+								<div class="dropdown-menu dropdown-info">
+									<a class="dropdown-item" href="#">Add new</a>
+									<a class="dropdown-item" href="#">Rename folder</a>
+									<a class="dropdown-item" href="#">Delete folder</a>
+								</div>
+							</div>
+
+							<!-- Heading -->
+							<a id="folder-1" data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+								<h5 class="mt-1 mb-0">
+							  <span>Folder 3</span>
+							  <i class="fas fa-angle-down rotate-icon"></i>
+							</h5>
+							</a>
+
+						</div>
+
+						<!-- Card body -->
+						<div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordionEx">
+							<div class="card-body">
+								Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+							</div>
+						</div>
+					</div>
+					<!-- Accordion card -->
+				</div>
+				<!--/.Accordion wrapper-->
 
 			</section>
 			<!--Section: Accordion-->
